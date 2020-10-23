8821e9fd3b270832999106efff88c1c7b18ca379
EmelianovaTG <emelianova.tg@phystech.edu>
2020-10-23 11:33:11 +0300

    Add Ball class

--- Lab 5/gun0.py
+++ Lab 5/gun0.py
@@ -8,7 +8,45 @@ GOLD = (255, 215, 0)
 SILVER = (192, 192, 192)
 
 class Ball():
-    pass
+    
+    def __init__(self, rad=40, vel=[0,0]):
+        self.coord = []
+        self.color = SILVER
+        self.rad = rad
+        self.vel = vel
+    
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, self.coord, self.rad)
+        
+    def flip_vel(self, axis, coef_perp=1.0, coef_par=1.0):
+        vel = np.array(self.vel)
+        n = np.array(axis)
+        n = n / n.norm()
+        vel_perp = vel*n
+        vel_par = vel - vel_perp
+        
+        vel_perp = -vel_perp*coef_perp
+        vel_par = vel_par*coef_par
+        vel = vel_perp + vel_par
+        
+        self.vel = vel.astype(np.int).tolist()
+        
+        
+        
+    def check_walls(self):
+        norm = [[1,0], [0,1]]
+        for i in range(2):
+            if self.coord[i] < self.rad:
+                self.coord[i] = self.rad + 1
+                self.flip_vel(norm[i])
+            if self.coord[i] > -self.rad + SCREEN_SIZE[i]:
+                self.coord[i] = -self.rad + SCREEN_SIZE[i] - 1
+                self.flip_vel(norm[i])
+        
+    def move(self, t_step=1.):
+        for i in range(2):
+            self.coord[i] += int(self.vel[i] * t_step)
+        self.check_walls()
 
 class Gun():
     
@@ -37,6 +75,8 @@ class Manager():
     def __init__(self):
         self.gun = Gun()
         self.score = Table()
+        self.balls = []
+        self.balls.append(Ball([100, 100], [10, 20]))
         
     def draw(self, screen):
         screen.fill(SCREEN_COLOR)
@@ -64,12 +104,14 @@ class Manager():
         done = self.handle_events(events)
         return done
     
+    def move(self):
+        for ball in self.balls:
+            ball.move()
 
         
 screen = pg.display.set_mode(SCREEN_SIZE)
 done = False
 clock = pg.time.Clock()
-
 mgr = Manager()
 
 while not done:
